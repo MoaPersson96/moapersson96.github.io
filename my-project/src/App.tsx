fa9297271fbaeee74e505b70dff045469078339e
MoaPersson96 <zowierandom@gmail.com>
2026-03-06 21:52:27 +0100

    Fixade resolution

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -9,112 +9,114 @@ import { WidePuffGrid } from "./patterns/WidePuffGrid";
 
 function App() {
   return (
-    <body className="min-h-screen flex flex-col max-w-[1920px] justify-self-center">
-      <Header />
-      {/* <Hero1
-      text = "Vi hjälper er att både etablera och utveckla er verksamhet i Skellefteåområdet"
-      navButtonText="Läs mer"
-      navDestination="e"
-      videoUrl="https://www.youtube.com/watch?v=TviBukuxiDw"
-      imgLink="https://stadsbyggnad.org/wp-content/uploads/sites/2/2022/05/skelleftea-880-700x466.jpg"
-      /> */}
-      <Hero2
-        date="2020-12-22"
-        smallHeaderText="Case Stories"
-        headerText="Campus Skellefteå säkerställer framtidens kunskapsbehov"
-        text="Hella Skellefteå vibrerar. Nya Miljardetableringar med batteritillverkaren Northvolt i spetsen får positiva effekter för näringslivet och det arbetas intensivt för att lösa alltifrån bostadsbehov till rekryteringsfrågor. Mitt i allt detta finns Campus Skellefteå, som får en nyckelroll för att möjliggöra utvecklingen. - Den industri som växer fram här är inte bara ny i Sverige utan hela Europa, vilket självklart påverkar kraven på både forskning och kompetensomvandling, säger Ida Lindh, strategiskt ansvarig på Campus"
-        navButtonText="Till Case Stories"
-        navDestination="Lorem Ipsum "
-        imgLink="https://a.travel-assets.com/findyours-php/viewfinder/images/res70/230000/230134-Skelleftea.jpg"
-        // videoUrl="https://www.youtube.com/watch?v=F5gFwiFIViQ"
-      />
+    <body className="min-h-screen flex flex-col">
+      <div className="max-w-[1920px] max-auto w-full">
+        <Header />
+        {/* <Hero1
+        text = "Vi hjälper er att både etablera och utveckla er verksamhet i Skellefteåområdet"
+        navButtonText="Läs mer"
+        navDestination="e"
+        videoUrl="https://www.youtube.com/watch?v=TviBukuxiDw"
+        imgLink="https://stadsbyggnad.org/wp-content/uploads/sites/2/2022/05/skelleftea-880-700x466.jpg"
+        /> */}
+        <Hero2
+          date="2020-12-22"
+          smallHeaderText="Case Stories"
+          headerText="Campus Skellefteå säkerställer framtidens kunskapsbehov"
+          text="Hella Skellefteå vibrerar. Nya Miljardetableringar med batteritillverkaren Northvolt i spetsen får positiva effekter för näringslivet och det arbetas intensivt för att lösa alltifrån bostadsbehov till rekryteringsfrågor. Mitt i allt detta finns Campus Skellefteå, som får en nyckelroll för att möjliggöra utvecklingen. - Den industri som växer fram här är inte bara ny i Sverige utan hela Europa, vilket självklart påverkar kraven på både forskning och kompetensomvandling, säger Ida Lindh, strategiskt ansvarig på Campus"
+          navButtonText="Till Case Stories"
+          navDestination="Lorem Ipsum "
+          imgLink="https://a.travel-assets.com/findyours-php/viewfinder/images/res70/230000/230134-Skelleftea.jpg"
+          // videoUrl="https://www.youtube.com/watch?v=F5gFwiFIViQ"
+        />
 
-      <main className="grow flex flex-col items-center justify-center text-center" role="main">
+        <main className="grow flex flex-col items-center justify-center text-center" role="main">
 
-        {/* Black and white puff */}
-        <div className="w-full max-w-6xl mx-auto px-4 mt-10 mb-10">
-          <WidePuffGrid
-            items={[
-              {
-                title: "Lorem Ipsum",
-                href: "#",
-                position: "left",
-              },
-              {
-                title: "Lorem Ipsum",
-                href: "#",
-                inverted: true,
-                position: "middle",
-              },
-              {
-                title: "Lorem Ipsum",
-                href: "#",
-                position: "right",
-              },
-            ]}
-          />
-        </div>
+          {/* Black and white puff */}
+          <div className="w-full max-w-6xl mx-auto px-4 mt-10 mb-10">
+            <WidePuffGrid
+              items={[
+                {
+                  title: "Lorem Ipsum",
+                  href: "#",
+                  position: "left",
+                },
+                {
+                  title: "Lorem Ipsum",
+                  href: "#",
+                  inverted: true,
+                  position: "middle",
+                },
+                {
+                  title: "Lorem Ipsum",
+                  href: "#",
+                  position: "right",
+                },
+              ]}
+            />
+          </div>
 
-        {/* Colored puffs */}
-        <div className="mt-16 w-full max-w-[1920px] mx-auto px-4">
-          <ColoredPuffGrid
-            items={[
-              {
-                title: "Lorem ipsum dolor sit amet.",
-                label: "Lorem",
-                href: "#",
-                arrowBackgroundColor: "#721904",
-              },
-              {
-                title: "Lorem ipsum dolor sit amet.",
-                label: "Lorem",
-                href: "#",
-                arrowBackgroundColor: "#7B932E",
-              },
-              {
-                title: "Lorem ipsum dolor sit amet.",
-                label: "Lorem",
-                href: "#",
-                arrowBackgroundColor: "#C1A016",
-              },
-              {
-                title: "Lorem ipsum dolor sit amet.",
-                label: "Lorem",
-                href: "#",
-                arrowBackgroundColor: "#455B81",
-              }
-            ]}
-          />
-        </div>
+          {/* Colored puffs */}
+          <div className="mt-16 w-full max-w-[1920px] mx-auto px-4">
+            <ColoredPuffGrid
+              items={[
+                {
+                  title: "Lorem ipsum dolor sit amet.",
+                  label: "Lorem",
+                  href: "#",
+                  arrowBackgroundColor: "#721904",
+                },
+                {
+                  title: "Lorem ipsum dolor sit amet.",
+                  label: "Lorem",
+                  href: "#",
+                  arrowBackgroundColor: "#7B932E",
+                },
+                {
+                  title: "Lorem ipsum dolor sit amet.",
+                  label: "Lorem",
+                  href: "#",
+                  arrowBackgroundColor: "#C1A016",
+                },
+                {
+                  title: "Lorem ipsum dolor sit amet.",
+                  label: "Lorem",
+                  href: "#",
+                  arrowBackgroundColor: "#455B81",
+                }
+              ]}
+            />
+          </div>
 
-        {/* Image Puff */}
+          {/* Image Puff */}
 
-        <div className="w-full max-w-[1920px] mx-auto px-4 mt-16 mb-16">
-          <ImagePuffGrid
-            items={[
-              {
-                label: "Lorem",
-                title: "Lorem ipsum dolor sit amet",
-                image: "/images/Bergnasbron.png",
-                href: "#",
-              },
-              {
-                label: "Lorem",
-                title: "Lorem ipsum dolor sit amet",
-                image: "/images/Norrsken.png",
-                href: "#",
-              },
-              {
-                label: "Lorem",
-                title: "Lorem ipsum dolor sit amet",
-                image: "/images/Kyrkbyn.png",
-                href: "#",
-              },
-            ]}
-          />
-        </div>
-        <Footer role="contentinfo" aria-label="Sidfot" />
-      </main>
+          <div className="w-full max-w-[1920px] mx-auto px-4 mt-16 mb-16">
+            <ImagePuffGrid
+              items={[
+                {
+                  label: "Lorem",
+                  title: "Lorem ipsum dolor sit amet",
+                  image: "/images/Bergnasbron.png",
+                  href: "#",
+                },
+                {
+                  label: "Lorem",
+                  title: "Lorem ipsum dolor sit amet",
+                  image: "/images/Norrsken.png",
+                  href: "#",
+                },
+                {
+                  label: "Lorem",
+                  title: "Lorem ipsum dolor sit amet",
+                  image: "/images/Kyrkbyn.png",
+                  href: "#",
+                },
+              ]}
+            />
+          </div>
+          <Footer role="contentinfo" aria-label="Sidfot" />
+        </main>
+      </div>
     </body>
   );
 }

8e03b14dc6995ebfc3e8bdce80f7eed18a12cc99
MoaPersson96 <zowierandom@gmail.com>
2026-03-10 02:56:18 +0100

    Fixade loggan och headern

--- my-project/src/patterns/WidePuffGrid.tsx
+++ my-project/src/patterns/WidePuffGrid.tsx
@@ -12,7 +12,15 @@ type Props = { items: PuffItem[] };
 
 export function WidePuffGrid({ items }: Props) {
     return (
-      <div className="w-full max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-3">
+      <div className="
+        w-full
+        max-w-5xl
+        mx-auto
+        grid
+        grid-cols-1
+        md:grid-cols-3
+        "
+        >
         {items.map((item, i) => (
             <WidePuff key={i} {...item} />
         ))}

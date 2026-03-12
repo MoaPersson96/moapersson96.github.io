2be03dc620b67ee1ec7abc602f71efc5670b494b
MoaPersson96 <zowierandom@gmail.com>
2026-03-12 13:32:09 +0100

    Fixade error med React Browser

--- my-project/src/main.tsx
+++ my-project/src/main.tsx
@@ -3,11 +3,21 @@ import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import Feed from './Routes/Feed.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+{
+  path: '/Feed',
+  element: <Feed/>,
+  errorElement: <div>404 Not Found</div>
+}])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HashRouter>
     <App />
     </HashRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 )

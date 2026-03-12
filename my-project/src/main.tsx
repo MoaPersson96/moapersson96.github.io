eb85858f71cb4c7ff9019d8c7e343ef09213b2c1
MoaPersson96 <zowierandom@gmail.com>
2026-03-12 11:37:00 +0100

    Fixade ERRORS

--- my-project/src/main.tsx
+++ my-project/src/main.tsx
@@ -1,25 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
-import Feed from './Routes/Feed.tsx'
-import Widgets from './Routes/Widgets.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-const router = createBrowserRouter([{
-  path: '/Widgets',
-  element: <Widgets/>,
-  errorElement: <div>404 Not Found</div>
-},
-{
-  path: '/Feed',
-  element: <Feed/>,
-  errorElement: <div>404 Not Found</div>
-}])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+    <App />
+    </HashRouter>
   </StrictMode>,
 )

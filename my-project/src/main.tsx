e29ee5b0bd06b302e14aa125fec1b61e63e9c046
MoaPersson96 <zowierandom@gmail.com>
2026-03-12 13:44:45 +0100

    Fixade HashBrowser för att få GH Pages att fungera

--- my-project/src/main.tsx
+++ my-project/src/main.tsx
@@ -1,23 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import Feed from './Routes/Feed.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-const router = createBrowserRouter([
-{
-  path: '/Feed',
-  element: <Feed/>,
-  errorElement: <div>404 Not Found</div>
-}])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HashRouter>
-    <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/Feed" element={<Feed />} />
+      </Routes>
     </HashRouter>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);

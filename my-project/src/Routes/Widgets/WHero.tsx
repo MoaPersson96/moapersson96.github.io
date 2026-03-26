94602be7b18bcb4765627d0a6bf28e5f2493038b
David <davidweddfelt1999@gmail.com>
2026-03-26 17:53:58 +0100

    Gjort hero 3

--- my-project/src/Routes/Widgets/WHero.tsx
+++ my-project/src/Routes/Widgets/WHero.tsx
@@ -1,9 +1,13 @@
-
+import Hero3 from "../../components/Hero3.tsx";
 
 function WHero() {
     return (
         <main className="grow flex flex-col items-center justify-center text-center" role="main">
-            {/* INNEHÅLLET HÄR */}
+            <Hero3
+                headerText="Hållbar digital utveckling"
+                text="Här hittar du inspirerande berättelser som gör livet med digitalisering enklare, arbetsdagen tryggare - och livet för många olika invånare i samhället mer meningsfullt."
+                imgLink="https://skelleftea.se/images/200.431c8a0f18c629e2b3832ad/1702909819786/(2)%20PD_220502_3643-Redigera.webp"
+            />
         </main>
 
     );

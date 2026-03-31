47c5157faed9b8a006808bb0d8149ee089e21c1f
David <davidweddfelt1999@gmail.com>
2026-03-31 14:20:42 +0200

    textblock fixade

--- my-project/src/Routes/Widgets/WText.tsx
+++ my-project/src/Routes/Widgets/WText.tsx
@@ -1,9 +1,22 @@
-
+import TextBox from "../../components/TextBox";
 
 function WText() {
     return (
         <main className="grow flex flex-col items-center justify-center text-center" role="main">
-            {/* INNEHÅLL HÄR */}
+            <TextBox
+                title="Möjligheterna på campus"
+                text="På Campus Skellefteå finns både Luleå tekniska universitet, Umeå universitet och ett nära samarbete med Mälardalens högskola och Folkuniversitetet. Dessutom kan du ta del av internationella universitets utbud via onlinekurser från några av världens bästa lärosäten.
+
+Det är ingen slump att spelindustrin har vuxit sig stark i Skellefteå. På Campus Skellefteå hittar du ett flertal utbildningar inom området och ett kreativt kluster med mötesplatser för spelutvecklare – bland annat Game Jams, föreläsningar, pitchtävlingar, resor till spelmässor och den årliga konferensen Arctic Game Conference.
+
+YH-utbildningarna är kortare än universitetsutbildningarna och tas fram i nära samarbete med företag i branscher som söker kompetent arbetskraft. På Campus Skellefteå finns bland annat utbildningar inom spel, IT och kreativa näringar, industri och tillverkning samt vård och samhälle. Här fungerar Campus som en nod i Vigeo – en ny test- och demonstrationsmiljö för digitalisering och digital infrastruktur för livslångt lärande."
+                background={true}
+            />
+            <TextBox
+                title="Studera"
+                text="Vuxnas motiv att börja studera är olika. Du kanske vill utveckla din yrkeskompetens och framskrida i karriären. En del önskar slutföra studier som kommit av sig och andra studerar för nöjes skull eller för att man har ett bestämt mål. Om du valt att studera på annan ort men bor i Skellefteå så kan du ha rätt till ett antal fördelar som pendlingsbidrag, möjlighet att skriva tenta eller annan hjälp i dina studier via Studentcentrum på Campus Skellefteå."
+                background={false}
+            />
         </main>
     );
 }
--- my-project/src/components/TextBox.tsx
+++ my-project/src/components/TextBox.tsx
@@ -1,17 +1,47 @@
+import infoImg from "../assets/images/info.png";
+
 type TextBoxProps = {
   title: string;
   text: string;
+  background?: boolean;
 };
 
-function TextBox({ text, title}: TextBoxProps) {
+function TextBox({ text, title, background }: TextBoxProps) {
   return (
-    <div className="bg-[#985A1C] text-white p-12 m-8 xl:mx-42 2xl:mx-96">
-      <h3 className="m-2">{title}</h3>
-      <p className="m-2"> {text}</p>
-    </div>
+    <div>
+      <div
+        className={`flex text-black text-left p-12 m-8 w-[45vw] ${
+          background ? "bg-gray-100" : ""
+        }`}
+      >
+        {background && (
+          <img
+            className="w-11 h-11 mr-12"
+            src={infoImg}
+            alt="info icon"
+          />
+        )}
 
+        <div>
+          <h3
+            className={`m-0 p-0 mb-8 font-extrabold ${
+              background ? "text-4xl" : "text-5xl"
+            }`}
+          >
+            {title}
+          </h3>
+
+          <p
+            className={`m-0 p-0 ${
+              background ? "text-base" : "text-xl"
+            }`}
+          >
+            {text}
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
-
 
 export default TextBox;

f9cf9feba5438dd81d6492274a228eb56c3277a2
David <davidweddfelt1999@gmail.com>
2026-04-02 15:59:38 +0200

    fixat text och bild komponenten

--- my-project/src/Routes/Widgets/WTextBild.tsx
+++ my-project/src/Routes/Widgets/WTextBild.tsx
@@ -1,9 +1,22 @@
-
+import TextImage from "../../components/text-image";
 
 function WTextBild() {
     return (
         <main className="grow flex flex-col items-center justify-center text-center" role="main">
-            {/* INNEHÅLL HÄR */}
+            <TextImage
+                image="https://skelleftea.se/images/200.661ce830197a1dc5fe4b25/1750841987293/Profilbild%20PLVM.jpg"
+                eyebrow="Jobba i Skellefteå"
+                title="Bygg framtiden med oss!"
+                description="Vi behöver dig och din kompetens. Med många jobb i platsbankerna finns det en hel del att välja mellan."
+                primaryCta={{
+                    text: "Vägar till en (ny) karriär",
+                    href: "/",
+                }}
+                secondaryCta={{
+                    text: "Upptäck alla lediga jobb",
+                    href: "/",
+                }}
+            />
         </main>
     );
 }

4e5ca400a91a195c6ac537fef73c4f23205199b4
MoaPersson96 <zowierandom@gmail.com>
2026-04-15 01:48:28 +0200

    Updates

--- assets/react-DR50SROH.js
+++ assets/react-DR50SROH.js
@@ -1,4 +1,4 @@
-import{n as e,r as t}from"./index-CIr_A7dE.js";var n=t(e(),1),r=`https://open.spotify.com`,i=/open\.spotify\.com\/(\w+)\/(\w+)/i,a=`https://open.spotify.com/embed-podcast/iframe-api/v1`,o=`SpotifyIframeApi`,s=`onSpotifyIframeApiReady`;function c(e,t={}){return`
+import{n as e,r as t}from"./index-C-v_nduF.js";var n=t(e(),1),r=`https://open.spotify.com`,i=/open\.spotify\.com\/(\w+)\/(\w+)/i,a=`https://open.spotify.com/embed-podcast/iframe-api/v1`,o=`SpotifyIframeApi`,s=`onSpotifyIframeApiReady`;function c(e,t={}){return`
     <style>
       :host {
         display: inline-block;
